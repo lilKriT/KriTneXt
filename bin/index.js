422d92959605538abe16da44abdfc3792e9f9b26
Piotr Filipiak <kiddhkane@gmail.com>
2023-10-18 21:16:51 +0200

    Basic Next installation.

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,9 @@
 #! /usr/bin/env node
 
+import { execa, $ } from "execa";
+
 console.log("Installing Next. The lilKriT way.");
+// const install = await execa("pnpm create", ["next-app@latest", "test"]);
+const next =
+  await $`pnpm create next-app@latest test --ts --eslint --tailwind --no-src-dir --app --import-alias @/*`;
+console.log("KriTneXt installed.");
