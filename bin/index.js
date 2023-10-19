710741a62c89e9442bdd9e4a8da17decfab80d13
Piotr Filipiak <kiddhkane@gmail.com>
2023-10-19 18:56:28 +0200

    Adding a basic library. Made a spinner work. Added settings note. Added a confirmation before installing.

--- bin/index.js
+++ bin/index.js
@@ -13,26 +13,31 @@ import {
   spinner,
   note,
 } from "@clack/prompts";
+import fs from "fs";
 
 // Greeting
-console.log("Installing Next. The lilKriT way.");
-intro("KriT neXt");
+// console.log("Installing Next. The lilKriT way.");
+intro("KriTneXt - starting.");
 
 // Getting arguments through clack
-const value = await text({
-  message: "Alright. How will your awesome app be called?",
+// Project name
+const projectName = await text({
+  message: "What's your project name? Pick a cool one.",
   placeholder: "my-kritical-app",
   initialValue: "my-kritical-app",
   validate(value) {
     if (value.length === 0) return "You must provide a name.";
+    if (fs.existsSync(value)) return "Folder already exists.";
   },
 });
 
-if (isCancel(value)) {
+if (isCancel(projectName)) {
   cancel("Operation canceled.");
   process.exit(0);
 }
 
+// There are just examples for now.
+/*
 const confirmed = await confirm({
   message: "Do you want to continue?",
 });
@@ -60,7 +65,7 @@ const additionalTools = await multiselect({
   required: false,
 });
 
-let settings = `text: ${value}   \nConfirmed: ${confirmed}   \nType: ${projectType}  \nTools: ${additionalTools}`;
+let settings = `text: ${projectName}   \nConfirmed: ${confirmed}   \nType: ${projectType}  \nTools: ${additionalTools}`;
 note(settings, "Settings");
 
 const s = spinner();
@@ -70,12 +75,34 @@ s.stop("Installed.");
 
 let nextSteps = "cd path    \ninstall   \ndev";
 note(nextSteps, "Next Steps");
+*/
 
-// Installing next
-// const nextInstall =
-//   await $`pnpm create next-app@latest test --ts --eslint --tailwind --no-src-dir --app --import-alias @/*`;
-console.log("Does this console log?");
+let settingsNote = `Name: ${projectName}`;
+note(settingsNote, "Settings:");
+
+const projectConfirmed = await confirm({
+  message: "Do you wish to continue with those settings?",
+});
+
+if (isCancel(projectConfirmed) || !projectConfirmed) {
+  cancel("Operation canceled.");
+  process.exit(0);
+}
+
+// Creating a spinner
+const s = spinner();
+
+// Installing Next
+s.start("Installing Next.");
+const nextInstall =
+  await $`pnpm create next-app@latest ${projectName} --ts --eslint --tailwind --no-src-dir --app --import-alias @/*`;
+s.stop("Next Installed.");
+
+// Installing an extra library
+s.start("Installing additional libraries.");
+process.chdir(projectName);
+await $`pnpm add lodash`;
+s.stop("Additiona libraries installed.");
 
 // Ending message
 outro("KriTTed.");
-console.log("KriTneXt installed.");
